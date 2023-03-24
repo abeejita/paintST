aa9ae1a3e9127148aebcdeec691ed6dfc7e88b8e
Abigail Curiel <a01655892@tec.mx>
2023-03-24 00:17:21 -0600

    Se optimizó y documentó el código.

--- paint.py
+++ paint.py
@@ -1,16 +1,33 @@
+# Juego de pintar basado en la colección de FreeGames
+# donde se hicieron modificaciones para una mejor experiencia
+# de juego.
+# Autores: Regina Luna, A01655821
+#          Diego Samperio, A01662935
+#          Abigail Curiel, A01655892
+# Fecha: 23/03/2023
+
+# Se importan las librerías que se utilizarán.
 from turtle import *
 from freegames import vector
 import math
 
+# Función que dibuja una línea desde un punto de 
+# comienzo hasta un punto de fin.
+# Toma como parámetro dos coordenadas de tipo 
+# (x, y), una de inicio y otra de fin de la figura.
+# No hay valor de retorno.
 def line(start, end):
-    "Draw line from start to end."
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
 
+# Función que dibuja un cuadrado desde un punto de 
+# comienzo hasta un punto de fin.
+# Toma como parámetro dos coordenadas de tipo 
+# (x, y), una de inicio y otra de fin de la figura.
+# No hay valor de retorno.
 def square(start, end):
-    "Draw square from start to end."
     up()
     goto(start.x, start.y)
     down()
@@ -22,11 +39,14 @@ def square(start, end):
 
     end_fill()
 
+# Función que dibuja un círculo desde un punto de 
+# comienzo hasta un punto de fin, el cual será
+# el radio del círculo.
+# Toma como parámetro dos coordenadas de tipo 
+# (x, y), una de inicio y otra de fin de la figura.
+# No hay valor de retorno.
 def makeCircle(start, end):
-    print(start)
-    print(end)
     radius = math.sqrt(pow(end.x - start.x, 2)+pow(end.y - start.y, 2))
-    print(radius)
     up()
     goto(start.x, start.y)
     down()
@@ -35,38 +55,50 @@ def makeCircle(start, end):
     circle(radius)
     end_fill()
 
+# Función que dibuja un rectángulo desde un punto de 
+# comienzo hasta un punto de fin como su diagonal.
+# Toma como parámetro dos coordenadas de tipo 
+# (x, y), una de inicio y otra de fin de la
+# diagonal de la figura.
+# No hay valor de retorno.
 def rectangle(start, end):
-    "Draw rectangle from start to end."
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
-    lado = (end.x - start.x)
+    lado1 = (end.x - start.x)
+    lado2 = (end.y - start.y)
 
     for count in range(2):
-        forward(lado)
+        forward(lado1)
         left(90)
-        forward(lado + lado)
+        forward(lado2)
         left(90)
 
     end_fill()
 
+# Función que dibuja un triángulo desde un punto 
+# de comienzo hasta un punto de fin que define 
+# la base y apotema.
+# Toma como parámetro dos coordenadas de tipo 
+# (x, y), una de inicio del triángulo y otra que
+# define la base y el apotema.
+# No hay valor de retorno.
 def triangle(start, end):
-    "Draw triangle from start to end."
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
-    lado = (end.x - start.x)
-
-    for count in range(3):
-        forward(lado)
-        left(120)
-
+    goto(end.x, start.y)
+    goto(start.x + (end.x - start.x)/2, end.y)
     end_fill()
 
+# Función que almacena un punto de inicio
+# o una forma de dibujo.
+# Toma como parámetro una coordenada de tipo 
+# (x, y).
+# No hay valor de retorno.
 def tap(x, y):
-    "Store starting point or draw shape."
     start = state['start']
 
     if start is None:
@@ -77,6 +109,11 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
+# Función que almacena un valor en un estado
+# de una llave/tecla particular.
+# Toma como parámetro un valor y la llave/tecla
+# en la que se guardará.
+# No hay valor de retorno.
 def store(key, value):
     "Store value in state at key."
     state[key] = value
@@ -85,13 +122,22 @@ state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
+
+# Se deshace una acción.
 onkey(undo, 'u')
+
+# Se agrega la posibilidad de usar distintos
+# colores para dibujar las formas de acuerdo
+# con una llave/tecla particular.
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('yellow'),'Y')
+
+# Se dibujan las distintas formas de acuerdo
+# con una llave/tecla particular.
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', makeCircle), 'c')
